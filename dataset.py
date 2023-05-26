4ee7315fce2f838845018c2fe1c59747fb2c077c
Vikram646 <2006018822@adit.ac.in>
2023-05-26 16:16:37 +0530

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,23 @@
+import typing
+
+import torch
+from tokenizer import Tokenizer
+
+
+class IntentDataset:
+    def __init__(self, intents, tags, tokenizer: Tokenizer) -> None:
+        self.intents = intents
+        self.tags = tags
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __getitem__(self, idx: int):
+        tokenized_dict = self.tokenizer.tokenize(self.intents[idx])
+        return {
+            "token_type_ids": torch.tensor(
+                tokenized_dict["token_type_ids"], dtype=torch.long
+            ),
+            "tag": torch.tensor(self.tags[idx], dtype=torch.long),
+        }
---
+++ intent.py
@@ -0,0 +1,9 @@
+import typing
+from dataclasses import dataclass
+
+
+@dataclass
+class IntentModel:
+    intent: str
+    text: typing.List[str]
+    responses: typing.List[str]
